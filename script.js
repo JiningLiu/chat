ebb86f9c94a669afca6d54d330ad1189d4be661a
Glitch (chatforschool) <none>
2022-09-20 16:26:01 +0000

    🎯🐬 Updated with Glitch

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+import { firebaseApp } from 'https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js';
+console.log(firebaseApp);
+
 var script = (function () {
   const firebase = window.firebase;
+  console.log(firebase);
   const firebaseConfig = {
     apiKey: "AIzaSyB7VotlTzZKJrqHsK0S7MOB0ORjWfNLb7k",
     authDomain: "school-chat-cbb2e.firebaseapp.com",
